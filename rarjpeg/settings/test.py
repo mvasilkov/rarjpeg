c259faa719043f568e21c2a025066c7ecbedd500
Mark Vasilkov <mvasilkov@gmail.com>
2014-02-14 23:07:40 +0200

    _Env util

--- rarjpeg/settings/test.py
+++ rarjpeg/settings/test.py
@@ -1,7 +1,8 @@
 from unittest import TestCase
-from .util import username
+from django.core.exceptions import ImproperlyConfigured
+from .util import username, env
 
-class UsernameTest(TestCase):
+class UtilTest(TestCase):
     def test_username(self):
         self.assertEqual(username('rememberingsteve@apple.com'),
                          'rememberingsteve+1eabe9c2')
@@ -9,3 +10,11 @@ class UsernameTest(TestCase):
                          'benedictxvi+1e33c678')
         self.assertEqual(username('"burn@the"@heretic'),
                          '"burn@the"+8f5d023b')
+
+    def test_env(self):
+        self.assertEqual(env.DJANGO_SETTINGS_MODULE, 'rarjpeg.settings')
+
+        self.assertIsInstance(env.HOME, str)
+
+        with self.assertRaises(ImproperlyConfigured):
+            res = env.WHARRGARBL
--- rarjpeg/settings/util.py
+++ rarjpeg/settings/util.py
@@ -4,3 +4,17 @@ import mmh3
 def username(email):
     return (email[:email.rindex('@')] +
             format(ctypes.c_uint32(mmh3.hash(email)).value, '+09x'))
+
+import os
+from django.core.exceptions import ImproperlyConfigured
+
+class _Env:
+    err = 'The environment variable {} is not set'
+
+    def __getattr__(self, attr):
+        try:
+            return os.environ[attr]
+        except KeyError:
+            raise ImproperlyConfigured(self.err.format(attr))
+
+env = _Env()
