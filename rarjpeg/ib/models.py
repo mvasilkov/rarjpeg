8dc6a5638acfba3996b01159c7118413f1259181
Mark Vasilkov <mvasilkov@gmail.com>
2014-02-17 00:17:32 +0200

    Memoization

--- rarjpeg/ib/models.py
+++ rarjpeg/ib/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from functools import lru_cache
 
 class Board(models.Model):
     uri = models.CharField(primary_key=True, max_length=20)
@@ -13,3 +14,7 @@ class Board(models.Model):
 
     class Meta:
         ordering = ('uri',)
+
+@lru_cache(1)
+def get_public_boards():
+    return Board.objects.filter(is_hidden=False)
--- rarjpeg/ib/test.py
+++ rarjpeg/ib/test.py
@@ -1,8 +1,14 @@
 from django.test import TestCase
-from .models import Board
+from .models import Board, get_public_boards
 
 class ModelTest(TestCase):
     def test_board(self):
         b = Board.objects.create(uri='a', name='Animu')
         self.assertEqual(format(b), '/a/ Animu')
         self.assertEqual(b.get_absolute_url(), '/a/')
+        self.assertCountEqual(get_public_boards(), [b])
+
+    def test_cache(self):
+        get_public_boards.cache_clear()
+        self.assertNumQueries(1, lambda: len(get_public_boards()))
+        self.assertNumQueries(0, lambda: len(get_public_boards()))
