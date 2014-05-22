e57ab34d7ffee4d51b42d61de62ee98355d27b97
Mark Vasilkov <mvasilkov@gmail.com>
2014-05-22 23:17:54 +0300

    test_load_crc_once

--- rarjpeg/ib/test.py
+++ rarjpeg/ib/test.py
@@ -6,6 +6,7 @@ from django.test.utils import override_settings
 from .models import Board, get_public_boards
 from .templatetags.ib_tags import load_crc, pub, public_boards
 
+
 class ModelTest(TestCase):
     def test_board(self):
         get_public_boards.cache_clear()
@@ -19,9 +20,11 @@ class ModelTest(TestCase):
         self.assertNumQueries(1, lambda: len(get_public_boards()))
         self.assertNumQueries(0, lambda: len(get_public_boards()))
 
+
 def _pub_re(path):
     return ('^' + re.escape(settings.STATIC_URL + path + '?crc=') +
             '[a-z0-9]{8}$')
+
 
 class HelperTest(TestCase):
     def test_load_crc(self):
@@ -30,6 +33,24 @@ class HelperTest(TestCase):
         with self.assertRaisesRegex(ImproperlyConfigured, 'Bad CRC file: '):
             load_crc(settings.OUR_ROOT.joinpath('requirements.txt'))
         self.assertIsInstance(load_crc(), dict)
+
+    def test_load_crc_once(self):
+        import rarjpeg.ib.templatetags.ib_tags as ib_tags
+        _load_crc = ib_tags.load_crc
+
+        def load_test(*args):
+            load_test.count += 1
+            return _load_crc(*args)
+
+        ib_tags.load_crc = load_test
+        load_test.count = 0
+
+        pub('WHARRGARBL')
+        self.assertEqual(load_test.count, 1)
+        pub('WHARRGARBL-96')
+        self.assertEqual(load_test.count, 1)
+
+        ib_tags.load_crc = _load_crc
 
     def test_pub(self):
         self.assertEqual(pub('WHARRGARBL'), settings.STATIC_URL + 'WHARRGARBL')
