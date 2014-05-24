21f1c51c7b1e6aa1a2b16bf2a5bc6f5d796d6e8d
Mark Vasilkov <mvasilkov@gmail.com>
2014-05-25 00:40:15 +0300

    assertLooksLikeASCII

--- rarjpeg/ib/__init__.py
+++ rarjpeg/ib/__init__.py
@@ -1,8 +1,10 @@
 from .models import Message
 
 
-def post_message(board, text, rt):
-    parent = (rt.parent if rt and rt.parent_id and not rt.get_next_sibling()
-              else rt)
-    return Message.objects.create(board=board, text=text, parent=parent,
-                                  reply_to=rt)
+def post_message(motive, **kwargs):
+    if motive:
+        kwargs.update(reply_to=motive, parent_id=motive.parent_id
+                      if motive.parent_id and not motive.get_next_sibling()
+                      else motive.pk)
+
+    return Message.objects.create(**kwargs)
--- rarjpeg/ib/test_msg_thread.py
+++ rarjpeg/ib/test_msg_thread.py
@@ -7,130 +7,124 @@ from .models import Board, Message
 CLEAN_RE = re.compile('^\n *|\n *$', flags=re.M)
 
 
-def _clean(s):
-    return CLEAN_RE.sub('', s).replace(' ', '')
+class MessageTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.board = Board.objects.create(uri='a', name='Animu')
 
+    @classmethod
+    def _msg(cls, *args, **kwargs):
+        return post_message(*args, board=cls.board, **kwargs)
 
-class MsgTestCase(TestCase):
-    def assertThreadString(self, any_msg, s):
-        Message.objects.rebuild()  # TODO WTF
+    def setUp(self):
+        self.a = [self.__class__._msg(False, text='@0')]
+
+    def msg(self, *args):
+        for n in args:
+            self.a.append(self._msg(self.a[n], text='@%d' % len(self.a)))
+
+    def assertLooksLikeASCII(self, s):
         res = []
-        for msg in Message.objects.filter(tree_id=any_msg.tree_id):
+        for msg in Message.objects.filter(tree_id=self.a[0].tree_id):
             res.append('>' * max(msg.level-1, 0) + msg.text)
 
-        self.assertEqual('\n'.join(res), _clean(s))
+        self.assertListEqual(res, CLEAN_RE.sub('', s).split())
 
-
-class MessageTest(MsgTestCase):
-    def setUp(self):
-        self.ib = Board.objects.create(uri='a', name='Animu')
-
+    # No replies
     def test_basic(self):
-        # No replies
-        a = post_message(self.ib, 'a', None)
+        self.assertLooksLikeASCII('@0')
 
-        self.assertIsNone(a.parent)
-        self.assertIsNone(a.reply_to)
-        self.assertThreadString(a, 'a')
-
+    # Replies to OP
     def test_linear(self):
-        # Replies to OP
-        a0 = post_message(self.ib, 'a0', None)
-        post_message(self.ib, 'a1', a0)
-        post_message(self.ib, 'a2', a0)
-        post_message(self.ib, 'a3', a0)
+        self.msg(0)
+        self.msg(0)
+        self.msg(0)
 
-        self.assertThreadString(a0, '''
-                                a0
-                                a1
-                                a2
-                                a3
-                                ''')
+        self.assertLooksLikeASCII('''
+                                  @0
+                                  @1
+                                  @2
+                                  @3
+                                  ''')
 
+    # Replies to last msg
     def test_linear2(self):
-        # Replies to last msg
-        a0 = post_message(self.ib, 'a0', None)
-        a1 = post_message(self.ib, 'a1', a0)
-        a2 = post_message(self.ib, 'a2', a1)
-        a3 = post_message(self.ib, 'a3', a2)
+        self.msg(0)
+        self.msg(1)
+        self.msg(2)
 
-        self.assertThreadString(a3, '''
-                                a0
-                                a1
-                                a2
-                                a3
-                                ''')
+        self.assertLooksLikeASCII('''
+                                  @0
+                                  @1
+                                  @2
+                                  @3
+                                  ''')
 
+    # Replies to OP and last msg
     def test_linear3(self):
-        # Replies to OP and last msg
-        a0 = post_message(self.ib, 'a0', None)
-        a1 = post_message(self.ib, 'a1', a0)
-        post_message(self.ib, 'a2', a1)
-        post_message(self.ib, 'a3', a0)
+        self.msg(0)
+        self.msg(1)
+        self.msg(0)
 
-        self.assertThreadString(a1, '''
-                                a0
-                                a1
-                                a2
-                                a3
-                                ''')
+        self.assertLooksLikeASCII('''
+                                  @0
+                                  @1
+                                  @2
+                                  @3
+                                  ''')
 
     def test_branching(self):
-        a0 = post_message(self.ib, 'a0', None)
-        a1 = post_message(self.ib, 'a1', a0)
-        post_message(self.ib, 'a2', a1)
-        post_message(self.ib, 'a3', a1)
-        post_message(self.ib, 'a4', a1)
+        self.msg(0)
+        self.msg(1)
+        self.msg(1)
+        self.msg(1)
 
-        self.assertThreadString(a0, '''
-                                a0
-                                a1
-                                >a3
-                                >a4
-                                a2
-                                ''')
+        self.assertLooksLikeASCII('''
+                                  @0
+                                  @1
+                                  >@3
+                                  >@4
+                                  @2
+                                  ''')
 
     def test_branching2(self):
-        a0 = post_message(self.ib, 'a0', None)
-        a1 = post_message(self.ib, 'a1', a0)
-        a2 = post_message(self.ib, 'a2', a0)
-        a3 = post_message(self.ib, 'a3', a1)
-        post_message(self.ib, 'a4', a1)
-        post_message(self.ib, 'a5', a3)
-        post_message(self.ib, 'a6', a3)
+        self.msg(0)
+        self.msg(0)
+        self.msg(1)
+        self.msg(1)
+        self.msg(3)
+        self.msg(3)
 
-        self.assertThreadString(a2, '''
-                                a0
-                                a1
-                                >a3
-                                >>a5
-                                >>a6
-                                >a4
-                                a2
-                                ''')
+        self.assertLooksLikeASCII('''
+                                  @0
+                                  @1
+                                  >@3
+                                  >>@5
+                                  >>@6
+                                  >@4
+                                  @2
+                                  ''')
 
     def test_branching3(self):
-        ib = self.ib
-        a = [post_message(ib, '@0', None)]
-        a.append(post_message(ib, '@1', a[0]))
-        a.append(post_message(ib, '@2', a[1]))
-        a.append(post_message(ib, '@3', a[1]))
-        a.append(post_message(ib, '@4', a[3]))
-        a.append(post_message(ib, '@5', a[1]))
-        a.append(post_message(ib, '@6', a[3]))
-        a.append(post_message(ib, '@7', a[3]))
-        a.append(post_message(ib, '@8', a[7]))
-        a.append(post_message(ib, '@9', a[4]))
+        self.msg(0)
+        self.msg(1)
+        self.msg(1)
+        self.msg(3)
+        self.msg(1)
+        self.msg(3)
+        self.msg(3)
+        self.msg(7)
+        self.msg(4)
 
-        self.assertThreadString(a[0], '''
-                                @0
-                                @1
-                                >@3
-                                >>@6
-                                >>@7
-                                >>@8
-                                >@4
-                                >>@9
-                                >@5
-                                @2
-                                ''')
+        self.assertLooksLikeASCII('''
+                                  @0
+                                  @1
+                                  >@3
+                                  >>@6
+                                  >>@7
+                                  >>@8
+                                  >@4
+                                  >>@9
+                                  >@5
+                                  @2
+                                  ''')
